8c6411746627c21f0e2f91077fdb8886c881faa9
Grigoriy <408813@mail.ru>
2020-02-24 14:40:23 +0300

    update getPosition

--- code.js
+++ code.js
@@ -13,35 +13,52 @@ for (const node of figma.currentPage.selection) {
     let gridSize = 4;
     // проеряем что у нас выбран именно фрейм
     if (node.type == 'FRAME') {
-        // проверяем что у фрейма настроеа сетка
+        // проверяем что у фрейма настроена сетка
         if (node.layoutGrids.length > 0 && node.layoutGrids[0].pattern == 'GRID') {
+            // если сетка есть, сохраняем ее в переменную gridSize
             gridSize = node.layoutGrids[0].sectionSize;
         }
     }
+    // уточняем что внутри  фрейма есть дети
     if ("children" in node) {
+        // запускаем цикл для каждого ребенка внутри фрейма
         for (const child of node.children) {
-            // console.log(getPosition(child.y, gridSize));
+            // и для каждого ребенка вызываем нашу функцию
             child.y = getPosition(child.y, gridSize);
         }
     }
 }
 figma.closePlugin();
 function getPosition(y, gridSize) {
-    // console.log(y % gridSize);
-    if (y % gridSize == 0) {
+    // сохраняем в переменную reminder остаток от деления отступа на размер сетки и заодно округляем значение до целого пикселя
+    let remainder = Math.round(y) % gridSize;
+    // проверяем делится ли отступ по высоте на размер сетки без остатка.
+    if (remainder == 0) {
+        // если делится, то возвращаем как есть
         return y;
+        // если нет, решаем куда двигать вверх или вниз
     }
-    else
-        return getPosition(y + 1, gridSize);
+    else {
+        // если остаток меньше или равен половине размера сетки, то возврашаем оступ минус остаток
+        if (remainder <= gridSize / 2) {
+            return y - remainder;
+            // если нет, тогда прибавляем к отступу разницу между размером сетки и остатком
+        }
+        else {
+            return y + (gridSize - remainder);
+        }
+    }
 }
 function setTime(node) {
     return __awaiter(this, void 0, void 0, function* () {
+        // узнаем текущее время и сохраняем его в переменную time
         let today = new Date();
         let time = today.getHours() + ":" + today.getMinutes();
-        console.log(time);
+        // находим первый тектовый блок, который называется Time
         let timeNode = node.findOne(n => n.name === "Time" && n.type === 'TEXT');
-        console.log(timeNode);
+        // загружаем шрифт для текстового блока перед тем как его изменить
         yield figma.loadFontAsync(timeNode.fontName).then(() => {
+            // меняем текст внутри текстового блока на текущее время
             timeNode.characters = time;
         });
     });
--- code.ts
+++ code.ts
@@ -7,11 +7,13 @@ for (const node of figma.currentPage.selection) {
   let gridSize = 4
 
 
-
   // проеряем что у нас выбран именно фрейм
   if (node.type == 'FRAME') {
-    // проверяем что у фрейма настроеа сетка
+
+    // проверяем что у фрейма настроена сетка
     if (node.layoutGrids.length > 0 && node.layoutGrids[0].pattern == 'GRID') {
+
+      // если сетка есть, сохраняем ее в переменную gridSize
       gridSize = node.layoutGrids[0].sectionSize
 
     }
@@ -20,19 +22,25 @@ for (const node of figma.currentPage.selection) {
 
 
 
-
+  // уточняем что внутри  фрейма есть дети
   if ("children" in node) {
+
+    // запускаем цикл для каждого ребенка внутри фрейма
     for (const child of node.children) {
 
-      // console.log(getPosition(child.y, gridSize));
-
-
+      // и для каждого ребенка вызываем нашу функцию
       child.y = getPosition(child.y, gridSize);
 
     }
   }
 
+
+
+
 }
+
+
+
 
 figma.closePlugin();
 
@@ -42,14 +50,31 @@ figma.closePlugin();
 
 
 
-
 function getPosition(y, gridSize) {
 
-  // console.log(y % gridSize);
+  // сохраняем в переменную reminder остаток от деления отступа на размер сетки и заодно округляем значение до целого пикселя
+  let remainder = Math.round(y) % gridSize
 
-  if (y % gridSize == 0) {
+  // проверяем делится ли отступ по высоте на размер сетки без остатка.
+  if (remainder == 0) {
+
+    // если делится, то возвращаем как есть
     return y
-  } else return getPosition(y + 1, gridSize);
+
+    // если нет, решаем куда двигать вверх или вниз
+  } else {
+
+    // если остаток меньше или равен половине размера сетки, то возврашаем оступ минус остаток
+    if (remainder <= gridSize / 2) {
+
+      return y - remainder
+
+      // если нет, тогда прибавляем к отступу разницу между размером сетки и остатком
+    } else {
+      return y + (gridSize - remainder)
+    }
+
+  }
 
 }
 
@@ -59,23 +84,23 @@ function getPosition(y, gridSize) {
 
 
 
+
 async function setTime(node) {
 
-
+  // узнаем текущее время и сохраняем его в переменную time
   let today = new Date();
   let time = today.getHours() + ":" + today.getMinutes();
 
 
-  console.log(time);
-
-
+  // находим первый тектовый блок, который называется Time
   let timeNode = node.findOne(n => n.name === "Time" && n.type === 'TEXT');
 
-  console.log(timeNode)
-
-
+  // загружаем шрифт для текстового блока перед тем как его изменить
   await figma.loadFontAsync(timeNode.fontName).then(() => {
+
+    // меняем текст внутри текстового блока на текущее время
     timeNode.characters = time
+
   })
 
 
